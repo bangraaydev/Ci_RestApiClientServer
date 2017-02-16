0186d17eb6f3cb3c321a837201e992c9770e56f1
bangraaydev <framcode@gmail.com>
2017-02-16 11:38:39 +0700

    add project rest api server

---
+++ rest_ci/kontak.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 16, 2017 at 01:23 AM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 7.0.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kontak`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `telepon`
+--
+
+CREATE TABLE `telepon` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `nomor` varchar(13) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `telepon`
+--
+
+INSERT INTO `telepon` (`id`, `nama`, `nomor`) VALUES
+(2, 'Mars oke', '08576666770'),
+(4, 'raden', '01842452'),
+(7, 'Alpha', '08576666765'),
+(8, 'Junior', '083475654');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `telepon`
+--
+ALTER TABLE `telepon`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `telepon`
+--
+ALTER TABLE `telepon`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
